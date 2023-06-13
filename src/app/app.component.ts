03c7a80c4356024f181e3f36052f8ce6bf35a4d1
SVV-Bonda <122274519+SVV-Bonda@users.noreply.github.com>
2023-06-13 17:24:32 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -8,18 +8,26 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  formsubmit = false;
 
   constructor(private fb: FormBuilder) {}
 
   testForm = this.fb.group({
-    name: [''],
-    phone: [''],
-    email: [''],
-    city: [''],
-    active: [''],
+    name: ['', Validators.required, Validators.pattern("^[a-zA-Z -']+")],
+    phone: [
+      '',
+      Validators.required,
+      Validators.pattern(/^\d{3}-\d{3}-\d{4}$/),
+      Validators.maxLength(10),
+    ],
+    email: ['', Validators.required, Validators.email],
+    group: ['', Validators.required],
+    city: ['', Validators.required],
+    check: ['', Validators.required],
   });
 
   SaveFun() {
-    console.log(this.testForm);
+    console.log(this.testForm.value);
+    this.formsubmit = true;
   }
 }
